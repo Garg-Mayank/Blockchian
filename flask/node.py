b21d16d47f9da817f42dd7e7dd35f113b4098745
Mayank Garg <42746774+Garg-Mayank@users.noreply.github.com>
2019-05-26 00:18:35 +0530

    Added transaction function

--- flask/node.py
+++ flask/node.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 from wallet import Wallet
@@ -17,6 +17,7 @@ def create_keys():
         global blockchain
         blockchain = Blockchain(wallet.public_key)
         response = {
+            'funds': blockchain.get_balance(),
             'public_key': wallet.public_key,
             'private_key': wallet.private_key
         }
@@ -52,8 +53,8 @@ def balance():
     balance = blockchain.get_balance()
     if balance != None:
         response = {
-            'message': "Balance Fetched Sucessfully",
-            'my_funds': balance
+            'my_funds': balance,
+            'message': "Balance Fetched Sucessfully"
         }
         return jsonify(response), 201
     else:
@@ -68,6 +69,48 @@ def balance():
 def get_UI():
     return 'Working'
 
+
+@app.route('/transaction', methods=['POST'])
+def add_transaction():
+    if wallet.public_key == None:
+        response = {
+            'Message': 'No data found'
+        }
+        return jsonify(response), 400
+    values = request.get_json()
+    if not values:
+        response = {
+            'message': "No data found"
+        }
+        return jsonify(response), 400
+    required_field = ['recipient', 'amount']
+    if not all(field for field in required_field):
+        response = {
+            'message': 'Required String missing'
+        }
+        return response, 400
+    recipient = values['recipient']
+    amount = values['amount']
+    signature = wallet.sign_transaction(wallet.public_key, recipient, amount)
+    success = blockchain.add_transaction(recipient, wallet.public_key, signature, amount)
+    if success:
+        response = {
+            'message': "Sucessfully added transaction",
+            'transaction': {
+                'sender': wallet.public_key,
+                'recipient': recipient,
+                'amount': amount,
+                'signature': signature
+            },
+            'funds': blockchain.get_balance()
+        }
+        return jsonify(response), 201
+        
+    else:
+        response = {
+            'message': "Creating a transaction failed"
+        }
+        return jsonify(response), 500
 
 @app.route('/mine', methods=['POST'])
 def mine():
