68f92e538765df34d57f92bd4e0bc38ecb1cdf74
Mayank Garg <42746774+Garg-Mayank@users.noreply.github.com>
2019-05-27 23:48:05 +0530

    Code Formatted as per pep8.

--- flask/utility/hash_utils.py
+++ flask/utility/hash_utils.py
@@ -20,5 +20,6 @@ def hash_block(block):
     hashable_block = block.__dict__.copy()
     hashable_block['transactions'] = [tx.to_ordered_dict()
                                       for tx in hashable_block['transactions']]
-    # Converting the "block" (which is dictionary) to 'string' and then encoding it to UTF-8.
+    # Converting the "block" (which is dictionary) to 'string' and then
+    # encoding it to UTF-8.
     return hash_string_256(json.dumps(hashable_block, sort_keys=True).encode())
--- flask/utility/verification.py
+++ flask/utility/verification.py
@@ -1,6 +1,4 @@
-"""
-Privides verification helper function.
-"""
+"""Privides verification helper function."""
 
 from utility.hash_utils import hash_block, hash_string_256
 from wallet import Wallet
@@ -13,34 +11,37 @@ class Verification:
         """Validate a proof.
 
         Arguments:
-            transactions: Transaction of the block 
+            transactions: Transaction of the block for which the proof is \
+            created.
             last_hash: The stored previous_hash.
-            proof: The proof we are testing. 
+            proof: The proof number we are testing.
         """
         guess = (str([tx.to_ordered_dict() for tx in transactions]) +
                  str(last_hash) + str(proof)).encode()
         # Hash the string.
         guess_hash = hash_string_256(guess)
         # Printing all the hashes performed.
-        #print(guess_hash)
+        # print(guess_hash)
         # Define the conditions for a new valid hash.
         return guess_hash[0:2] == "00"
 
     @classmethod
     def verify_chain(cls, blockchain):
-        """Verifies whether the block are matching or not."""
+        """Verify the current blockchain and return True if it's valid, False
+        otherwise"""
         for index, block in enumerate(blockchain):
             if index == 0:
                 continue
             if block.previous_hash != hash_block(blockchain[index - 1]):
                 return False
-            if not cls.valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
+            if not cls.valid_proof(block.transactions[:-1],
+                                   block.previous_hash, block.proof):
                 print("Proof of work is Invalid!!!")
                 return False
         return True
 
     @staticmethod
-    def verify_transaction(transaction, get_balance, check_funds = True):
+    def verify_transaction(transaction, get_balance, check_funds=True):
         """Verifies whether transaction is possible or not.
 
         Arguments:
@@ -50,11 +51,13 @@ class Verification:
         if check_funds:
             sender_balance = get_balance(transaction.sender)
             print(sender_balance)
-            return sender_balance >= transaction.amount and Wallet.verify_transaction(transaction)
+            return (sender_balance >= transaction.amount and
+                    Wallet.verify_transaction(transaction))
         else:
             return Wallet.verify_transaction(transaction)
 
     @classmethod
-    def verify_transactions(cls, open_transaction, get_balance):
+    def verify_transactions(cls, open_transactions, get_balance):
         """Verifies all the transactions."""
-        return all([cls.verify_transaction(tx, get_balance, False) for tx in open_transaction])
+        return all([cls.verify_transaction(tx, get_balance, False)
+                    for tx in open_transactions])
